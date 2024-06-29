005e7e3f4e058a088802b045f092a1ccb9e3d691
unknown <brenaairotiv18@gmail.com>
2024-06-28 21:04:55 -0300

    Criado o componente header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from "./components/template/header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: 'app.component.html'
- 
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: 'app.component.html',
+    imports: [
+        RouterOutlet,
+        MatToolbarModule,
+        HeaderComponent
+    ]
 })
 export class AppComponent {
   title = 'bookstore-front';
