0e84cb11fa70cac1ada541bc2581ecae634d8775
unknown <brenaairotiv18@gmail.com>
2024-07-11 20:53:46 -0300

    Consumindo api do tipo POST_CATEGORIA e TRATAMENTO DE ERROS

--- src/app/components/views/categoria/categoria-create/categoria-create.component.ts
+++ src/app/components/views/categoria/categoria-create/categoria-create.component.ts
@@ -3,6 +3,10 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
+import { CategoriaService } from '../../categoria.service';
+import { Categoria } from '../categoria-read/categoria.model';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-categoria-create',
@@ -11,11 +15,31 @@ import {MatButtonModule} from '@angular/material/button';
     MatInputModule,
     MatFormFieldModule,
     FormsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule
   ],
   templateUrl: './categoria-create.component.html',
   styleUrl: './categoria-create.component.css'
 })
 export class CategoriaCreateComponent {
+
+  categoria: Categoria = {
+    nome: '',
+    descricao: ''
+  }
+  constructor(private service: CategoriaService, private router: Router){}
+
+  create(): void{
+    this.service.create(this.categoria).subscribe((resposta) => {
+      this.router.navigate(['categorias'])
+      this.service.mensagem('Categoria criada com sucesso!');
+    }, err => {
+      for(let i = 0; i < err.error.errors.length; i++){
+        this.service.mensagem(err.error.errors[i].mensagem)
+      };
+      
+    })
+    
+  }
 
 }
